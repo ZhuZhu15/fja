998ee637d89066e2d3ad99f2dae8db147572f2d1
sergey <pain24@mail.ru>
2018-06-13 15:24:39 +0300

    header icon

--- header.php
+++ header.php
@@ -3,10 +3,11 @@
 
 <!DOCTYPE html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>Kanken Shop</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="shortcut icon" href="/img/fjall.png" type="image/png">
   
   <script src="js/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
